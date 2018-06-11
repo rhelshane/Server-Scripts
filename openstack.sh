31f68b2d491a054e5cb05e18ed16f4c38bedf736
Shane Sexton <38745776+rhelshane@users.noreply.github.com>
2018-06-11 12:37:28 -0700

    Update openstack.sh

--- openstack.sh
+++ openstack.sh
@@ -1,17 +1,19 @@
 #!/bin/bash
 
-apt remove lxd lxd-client ufw
-
+# Disable IPv6
 echo "net.ipv6.conf.all.disable_ipv6 = 1" >> /etc/sysctl.conf
-
 echo "net.ipv6.conf.default.disable_ipv6 = 1" >> /etc/sysctl.conf
-
 echo "net.ipv6.conf.lo.disable_ipv6 = 1" >> /etc/sysctl.conf
 
+# Package maintenance
+apt remove lxd lxd-client ufw
 apt update
-
 apt upgrade
 
+# Install Snaps
 snap install lxd
+snap install conjure-up --classic
 
-lxd init
+
+
+
