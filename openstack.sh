fea801c44c6c690ae55418b147764268f529c80c
Shane Sexton <38745776+rhelshane@users.noreply.github.com>
2018-06-12 07:46:51 -0700

    Update openstack.sh

--- openstack.sh
+++ openstack.sh
@@ -1,9 +1,31 @@
 #!/bin/bash
 
+INTERFACE="/etc/network/interfaces";
+IPADDR="192.168.192.3";
+NETMASK="255.255.255.0";
+GATEWAY="192.168.192.2";
+DNSA="1.1.1.1";
+DNSB="8.8.8.8";
+
 # Disable IPv6
 echo "net.ipv6.conf.all.disable_ipv6 = 1" >> /etc/sysctl.conf
 echo "net.ipv6.conf.default.disable_ipv6 = 1" >> /etc/sysctl.conf
 echo "net.ipv6.conf.lo.disable_ipv6 = 1" >> /etc/sysctl.conf
+
+# Disable Services
+systemctl stop ufw
+systemctl disable ufw
+systemctl mask ufw
+
+# Configure IPv4
+if [ -e "$INTERFACE" ]
+then 
+  sed "s/^iface.*dhcp.*$/iface\ ens32\ inet\ static/g" $INTERFACE;
+  echo "	address $IPADDR" >> $INTERFACE;
+  echo "	netmask $NETMASK" >> $INTERFACE;
+  echo "	gateway $GATEWAY" >> $INTERFACE;
+  echo "	dns-nameservers $DNSA $DNSB" >> $INTERFACE;
+fi
 
 # Package maintenance
 apt remove -y lxd lxd-client ufw
