1ee4a4fcda929844b64d60552534efc383abbfc4
Shane Sexton <redhatshane@protonmail.com>
2018-06-20 11:09:04 -0700

    quieted output

--- GNS3/setup.sh
+++ GNS3/setup.sh
@@ -22,10 +22,10 @@ SSH_PORT="22"
 # Update packages via apt and install gns3-server via pip3
 function installGNS3()
 {
-  apt update -y
-  apt upgrade -y
-  apt install -y $(cat packagelist)
-  pip3 install gns3-server==$GNS_VERS
+  apt update -y -q
+  apt upgrade -y -q
+  apt install -y -q $(cat packagelist)
+  pip3 install -q gns3-server==$GNS_VERS
 }
 
 
@@ -33,15 +33,15 @@ function installGNS3()
 function installDynamips()
 {
   dpkg --add-architecture i386
-  apt update -y
-  apt install -y dynamips:i386
+  apt update -y -q
+  apt install -y -q dynamips:i386
 }
 
 
 # Clone, build and install ubridge
 function installUbridge()
 {
-  git clone https://github.com/GNS3/ubridge.git || echo "Error cloning ubridge."
+  git clone -q https://github.com/GNS3/ubridge.git || echo "Error cloning ubridge."
   cd ubridge
   make
   make install
