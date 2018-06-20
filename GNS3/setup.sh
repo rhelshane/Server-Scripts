73999c24bf22550f4ccb1493290e2b50418d1cfc
Shane Sexton <redhatshane@protonmail.com>
2018-06-20 09:18:34 -0700

    fixed directory issue

--- GNS3/setup.sh
+++ GNS3/setup.sh
@@ -57,6 +57,9 @@ function configureGNS3()
   sed -i "s/___USER___/$GNS_USER/" GNS3.conf
   sed -i "s/___PASS___/$GNS_PASS/" GNS3.conf	
   sed -i "s/___PORT___/$GNS_PORT/" GNS3.conf
+  sed -i "s/___IMAGE_DIR___/$IMAGE_DIR/" GNS3.conf
+  sed -i "s/___APPLIANCE_DIR___/$APPLIANCE_DIR/" GNS3.conf
+  sed -i "s/___PROJECT_DIR___/$PROJECT_DIR/" GNS3.conf
   cp GNS3.conf $HOME/.config/
 }
 
