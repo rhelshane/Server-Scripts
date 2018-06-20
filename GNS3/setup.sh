8a703e8d07e8e7b7f15d14cadc22b8f8c37511f0
Shane Sexton <redhatshane@protonmail.com>
2018-06-19 18:03:14 -0700

    Enabled UFW

--- GNS3/setup.sh
+++ GNS3/setup.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
-#This script automates the installation of GNS3.
+# This script automates the installation of a GNS3 Server on Ubuntu 18.04 LTS.
+# Ensure the directories and credentials specified in the variables below are 
+# what you want.
 
-IMAGE_DIR="/root/GNS3/Images"
-APPLIANCE_DIR="/root/GNS3/Appliances"
-PROJECT_DIR="/root/GNS3/Project"
+CURRENT_DIR=$(pwd)
+IMAGE_DIR="$HOME/GNS3/Images"
+APPLIANCE_DIR="$HOME/GNS3/Appliances"
+PROJECT_DIR="$HOME/GNS3/Project"
 GNS_USER="student88"
 GNS_PASS="QED314"
 
@@ -19,36 +22,42 @@ function installDynamips()
 {
   dpkg --add-architecture i386
   apt update -y
-  apt install dynamips:i386
+  apt install -y dynamips:i386
 }
 
 function installUbridge()
 {
-  apt install -y libpcap
   git clone https://github.com/GNS3/ubridge.git
   cd ubridge
   make
   make install
-  cd ..
+  cd $CURRENT_DIR
 }
 
-function disableUFW()
+function configUFW()
 {
-  systemctl stop ufw
-  systemctl disable ufw
+
+  ufw allow 22
+  ufw allow 3080
+  ufw allow 5000:6000/tcp
+  ufw allow 5000:6000/udp
+  ufw allow 10000:11000/tcp
+  ufw allow 10000:11000/udp
+  systemctl enable ufw
+  ufw enable
 }
 
 function configureGNS3()
 {
-  mkdir /root/GNS3 /root/.config
+  mkdir $HOME/GNS3 /root/.config
   mkdir $IMAGE_DIR $APPLIANCE_DIR $PROJECT_DIR
   sed -i "s/___USER___/$GNS_USER/" GNS3.conf
   sed -i "s/___PASS___/$GNS_PASS/" GNS3.conf	
-  cp GNS3.conf /root/.config/
+  cp GNS3.conf $HOME/.config/
 }
 
 installGNS3
 installDynamips
 installUbridge
-disableUFW
+configUFW
 configureGNS3
